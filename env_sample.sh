55f6087bdf2f6dd9169689370e892ee9afcde1e0
tevinric <tevinric@gmail.com>
2026-02-06 21:40:23 +0200

    add env sample

---
+++ env_sample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Environment variables for development
+
+export DB_HOST=""
+export DB_PORT="5432"
+export DB_NAME=""
+export DB_USER=""
+export DB_PASSWORD=""
+export FRONTEND_PORT=3002
+export BACKEND_PORT=5002
+export SECRET_KEY=""
+#export FLASK_ENV="development"
+export FLASK_ENV=""
+export AZURE_TENANT_ID=""
+export AZURE_CLIENT_ID=""
+# export AZURE_REQUIRED_GROUP_ID="your-group-id"
